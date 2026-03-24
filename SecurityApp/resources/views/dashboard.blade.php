762b23dd028bdaf0e2f4911f146746907fddf474
josemario234 <AL02963295@tecmilenio.mx>
2026-03-23 21:15:50 -0700

    Creating "dashboard.blade.php"

---
+++ SecurityApp/resources/views/dashboard.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class=container>
+    <div class="py-5 text-center">
+        <h1>Dashboard</h1>
+        <p class="lead">Authenticated user dasboard. Only logged-in users can see this content.</p>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card border-success">
+                <div class="card-healer">Welcome, {{ Auth::user()->name}}</div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

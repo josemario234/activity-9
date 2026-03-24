9443eda78e564be90442d0c0684709568ed5091d
josemario234 <AL02963295@tecmilenio.mx>
2026-03-23 21:24:51 -0700

    Creating "landing.blade.php"

---
+++ SecurityApp/resources/views/landingpage.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="py-5 text-center">
+        <h1>Landing Page</h1>
+        <p class="lead">Welcome to the public landing page.</p>
+        <p>Use Login or Register from the navbar to access the dashboard.</p>
+    </div>
+    <div class="row">
+        <div class="card border-primary">
+            <div class="card-body">
+                <h4 class="card-title">Guest Area</h4>
+                <p class="card-text">Content clearly annotated as the guest landing page.</p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
